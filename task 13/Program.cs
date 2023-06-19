800330a3a0fedadd5a218d01453c5fa31b73a065
Kamilya <you@example.com>
2023-06-19 19:42:19 +0300

    Это не решение, а набросок мыслей

---
+++ task 13/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+645 -> 5
+78 -> третьей цифры нет
+32679345 -> 6*/
+
+System.Console.WriteLine("Enter number");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 99 && numberA <999)
+{
+    numberA = numberA % 10;
+    System.Console.WriteLine(numberA);
+}
+else if (numberA < 99)
+{
+    System.Console.WriteLine("Attention! No third digit");
+}
+while (numberA >1000)
+    {
+        numberA = numberA / 10;
+    }
+    numberA = numberA % 10;
+
+
