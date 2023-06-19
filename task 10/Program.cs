9d372e388784736ccd665fac3cd6f65d8b632715
Kamilya <you@example.com>
2023-06-19 17:24:56 +0300

    Решение задачи 10

---
+++ task 10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Enter number 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 99 && numberA < 1000)
+{
+    int numberB = ((numberA % 100)/10);
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.WriteLine("Enter correction numbers");
+}
